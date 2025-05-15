35bccd7ab2c3f7d220d812f536ba47bc5d6abb9c
ArgaFd <bagusarga3@gmail.com>
2025-05-15 22:44:28 +0700

    feat: add logging middleware, CORS, and info API endpoint

--- main.go
+++ main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+// Logger middleware
+func Logger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		duration := time.Since(start)
+
+		fmt.Printf("[%s] %s %s - %v\n",
+			c.Method(),
+			c.Path(),
+			c.IP(),
+			duration,
+		)
+		return err
+	}
+}
 
 func main() {
 	// Create new Fiber app
 	app := fiber.New()
 
+	// Use logger middleware
+	app.Use(Logger())
+
+	// Configure CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	// Define a route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	// Add a simple API endpoint
+	app.Get("/api/info", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"app_name":  "Simple API",
+			"version":   "1.0.0",
+			"timestamp": time.Now().Format(time.RFC3339),
+			"status":    "running",
+		})
 	})
 
 	// Start server
