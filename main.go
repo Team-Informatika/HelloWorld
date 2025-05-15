093e8b0fa7728325b72d01c74d16f967ad004346
ArgaFd <bagusarga3@gmail.com>
2025-05-15 22:24:49 +0700

    feat: add logging middleware and user endpoint

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+// Logger middleware
+func Logger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		duration := time.Since(start)
+
+		fmt.Printf("[%s] %s %s - %v\n",
+			c.Method(),
+			c.Path(),
+			c.IP(),
+			duration,
+		)
+		return err
+	}
+}
+
 func main() {
 	// Create new Fiber app
 	app := fiber.New()
+
+	// Use logger middleware
+	app.Use(Logger())
 
 	// Define a route
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -21,6 +43,16 @@ func main() {
 			"status":  "active",
 			"message": "Server is running",
 			"version": "1.0.0",
+		})
+	})
+
+	// Add user info endpoint
+	app.Get("/api/user/:id", func(c *fiber.Ctx) error {
+		userID := c.Params("id")
+		return c.JSON(fiber.Map{
+			"user_id":    userID,
+			"name":       "User " + userID,
+			"created_at": time.Now().Format(time.RFC3339),
 		})
 	})
 
